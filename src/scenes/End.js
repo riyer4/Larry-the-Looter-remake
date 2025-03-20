2d7c10c0fb61631402234dfc14a37be9b6a7792c
Ria Iyer <riyer4@ucsc.edu>
2025-03-19 18:25:33 -0700

    minor fix

--- src/scenes/End.js
+++ src/scenes/End.js
@@ -31,10 +31,35 @@ class End extends Phaser.Scene {
 
         this.finalScoreText = this.add.text(250, 300, `${this.finalScore}`, finalScoreConfig).setOrigin(0, 0)
 
+
+        // back to main menu
+
+        keyMENU = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M)
+
+        let controlsConfig = {
+            fontFamily: 'Times New Roman',
+            fontSize: '18px',
+            backgroundColor: '#f0f14e',
+            color: '#000',
+            allig: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+
+            fixedWidth: 0
+        }
+
+        this.add.text(320, 400, '[M] -> Menu', controlsConfig).setOrigin(0.5)
+
     }
 
     update() {
 
+        if (Phaser.Input.Keyboard.JustDown(keyMENU)) {
+
+            this.scene.start('menuScene')    
+        }
     }
 
 
